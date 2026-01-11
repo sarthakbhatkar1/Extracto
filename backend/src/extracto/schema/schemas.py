62b78349a2cbcea89d082d954be657844dd98658
sarthakbhatkar1 <sarthakbhatkar1@users.noreply.github.com>
2026-01-11 23:23:45 +0530

    added password and names validations

--- backend/src/extracto/schema/schemas.py
+++ backend/src/extracto/schema/schemas.py
@@ -1,7 +1,7 @@
-# auth/schemas.py
-from typing import Optional
+import re
 
-from pydantic import BaseModel, EmailStr
+from typing import Optional
+from pydantic import BaseModel, EmailStr, field_validator
 
 from extracto.utils.util import RoleEnum
 
@@ -9,9 +9,53 @@ from extracto.utils.util import RoleEnum
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
-    firstName: Optional[str]
-    lastName: Optional[str]
+    firstName: Optional[str] = None
+    lastName: Optional[str] = None
     role: str = RoleEnum.USER
+
+
+    @field_validator
+    @classmethod
+    def validate_password_strength(cls, value: str) -> str:
+        """
+        Validate password strength:
+        - At least 8 characters
+        - At least on uppercase letter
+        - At least one lowercase letter
+        - At least one digit
+        - At least one special character
+        """
+
+        if len(value) < 8:
+            raise ValueError("Password must be at least 8 characters long.")
+        
+        if not re.search(r'[A-Z]', value):
+            raise ValueError('Password must contain at least one uppercase letter.')
+        
+        if not re.search(r'[A-Z]', value):
+            raise ValueError('Password must contain at least one lowercase letter.')
+        
+        if not re.search(r'\d', value):
+            raise ValueError('Password must contain at least one digit letter.')
+        
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+            raise ValueError('Password must contain at least one special letter.')
+
+    @field_validator
+    @classmethod
+    def sanitize_name(cls, value: Optional[str]) -> Optional[str]:
+        """
+        Sanitize name inputs to prevent injection attacks.
+        
+        :param cls: Description
+        :param value: Description
+        :type value: Optional[str]
+        :return: Description
+        :rtype: str | None
+        """
+
+        value = re.sub(r'[<>\"\'`;]', '', value)
+        return value.strip()[:256]
 
 
 class UserOut(BaseModel):
